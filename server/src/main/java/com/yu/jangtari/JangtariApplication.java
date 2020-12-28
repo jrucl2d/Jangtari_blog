6c07fa88639124826b1000faff7b3cc7de5c53b9
jrucl2d <ysk789465@gmail.com>
2020-12-28 14:26:29 +0900

    Initial commit

---
+++ server/src/main/java/com/yu/jangtari/JangtariApplication.java
@@ -0,0 +1,13 @@
+package com.yu.jangtari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangtariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JangtariApplication.class, args);
+    }
+
+}
