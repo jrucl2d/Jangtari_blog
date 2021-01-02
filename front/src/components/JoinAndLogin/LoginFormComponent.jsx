c799f1f7773a4f1842e379491e53882723d17b6f
jrucl2d <ysk789465@gmail.com>
2021-01-02 23:01:46 +0900

    로그인, 로그아웃 구현 완료

--- front/src/components/JoinAndLogin/LoginFormComponent.jsx
+++ front/src/components/JoinAndLogin/LoginFormComponent.jsx
@@ -1,30 +1,81 @@
-import React from 'react'
-import './LoginAndJoin.css'
-import { Form, Button } from 'react-bootstrap'
+import React, { useState } from "react";
+import "./LoginAndJoin.css";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
 
-function LoginFormComponent() {
-    return (
-        <div>
-            <h1>로그인</h1>
-            <Form className="join-login-form">
-                <Form.Group controlId="username">
-                    <Form.Label>아이디</Form.Label>
-                    <Form.Control type="text" placeholder="아이디 입력" />
-                </Form.Group>
+function LoginFormComponent({ history }) {
+  const [info, setInfo] = useState({
+    username: "",
+    password: "",
+  });
 
-                <Form.Group controlId="password">
-                    <Form.Label>비밀번호</Form.Label>
-                    <Form.Control type="password" placeholder="비밀번호 입력" />
-                </Form.Group>
-                <Button variant="outline-primary" type="submit">
-                    로그인
-                </Button>
-                <Button variant="outline-success" type="submit">
-                    회원가입
-                </Button>
-            </Form>
-        </div>
-    )
+  const onChangeInfo = (e) => {
+    setInfo({
+      ...info,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onClickSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("/login", {
+        username: info.username,
+        password: info.password,
+      });
+      // Access token이 만료되어 refreshToken으로 새로운 accessToken을 받았을 경우
+      const role = result.data.result;
+      localStorage.setItem("role", role);
+      if (role === "ADMIN") {
+        alert("장따리 왔누 ㅋㅋㅋㅋ");
+      } else {
+        alert("로그인했습니다.");
+      }
+      history.push("/");
+    } catch (err) {
+      alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <h1>로그인</h1>
+      <Form className="join-login-form">
+        <Form.Group controlId="username">
+          <Form.Label>아이디</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="아이디 입력"
+            onChange={onChangeInfo}
+            name="username"
+            value={info.username}
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>비밀번호</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="비밀번호 입력"
+            onChange={onChangeInfo}
+            name="password"
+            value={info.password}
+          />
+        </Form.Group>
+        <Button variant="outline-primary" type="button" onClick={onClickSubmit}>
+          로그인
+        </Button>
+        <Button
+          variant="outline-success"
+          type="button"
+          onClick={() => {
+            history.push("/joinForm");
+          }}
+        >
+          회원가입
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
-export default LoginFormComponent
+export default LoginFormComponent;
--- front/src/components/MainPage/SideMenuComponent.jsx
+++ front/src/components/MainPage/SideMenuComponent.jsx
@@ -1,51 +1,87 @@
-import React, { useRef, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { getAllCategories } from '../../modules/categoryReducer';
+import axios from "axios";
+import React, { useRef, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { getAllCategories } from "../../modules/categoryReducer";
 
 function SideMenuComponent({ isToggle, onClickMenuButton }) {
-    const dispatch = useDispatch();
-    const { categories } = useSelector(
-        (state) => state.categoryReducer
-    );
-    const menuRef = useRef(null);
-    useEffect(() => {
-        if (categories === null) {
-            dispatch(getAllCategories());
-        }
-        // eslint-disable-next-line
-    }, [])
-    useEffect(() => {
-        if (isToggle) {
-            menuRef.current.classList.add("show")
-        } else {
-            menuRef.current.classList.remove("show")
-        }
-    }, [isToggle])
+  const dispatch = useDispatch();
+  const { categories } = useSelector((state) => state.categoryReducer);
+  const menuRef = useRef(null);
+  useEffect(() => {
+    if (categories === null) {
+      dispatch(getAllCategories());
+    }
+    // eslint-disable-next-line
+  }, []);
+  useEffect(() => {
+    if (isToggle) {
+      menuRef.current.classList.add("show");
+    } else {
+      menuRef.current.classList.remove("show");
+    }
+  }, [isToggle]);
 
+  const onClickLogout = async (e) => {
+    try {
+      await axios.get("/signout");
+      localStorage.removeItem("role");
+      alert("로그아웃 하였습니다.");
+    } catch (err) {
+      alert("문제가 발생했습니다. 잠시 뒤에 다시 시도해주세요.");
+      return;
+    }
+    onClickMenuButton();
+  };
 
-    return (
-        <>
-            <div className="side-bar" ref={menuRef}>
-                <h2>장따리의 똥글</h2>
-                <ul className="side-bar-user">
-                    <li><Link onClick={() => onClickMenuButton()} to="/">메인으로</Link></li>
-                    <li><Link onClick={() => onClickMenuButton()} to="/about">Who is '장딱'</Link></li>
-                    <li><Link onClick={() => onClickMenuButton()} to="/loginForm">로그인</Link></li>
-                    <li><Link onClick={() => onClickMenuButton()} to="/joinForm">회원가입</Link></li>
-                </ul>
-                <h2>카테고리</h2>
-                <ul className="side-bar-list">
-                    {categories && categories.map((v) => (
-                        <li key={v.id}><Link to="/#">
-                            {v.name}
-                        </Link></li>
-                    ))}
-                </ul>
-            </div>
-            <div className="side-bar-curtain" onClick={() => onClickMenuButton()} />
-        </>
-    )
+  return (
+    <>
+      <div className="side-bar" ref={menuRef}>
+        <h2>장따리의 똥글</h2>
+        <ul className="side-bar-user">
+          <li>
+            <Link onClick={() => onClickMenuButton()} to="/">
+              메인으로
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => onClickMenuButton()} to="/about">
+              Who is '장딱'
+            </Link>
+          </li>
+          {localStorage.getItem("role") === null ? (
+            <li>
+              <Link onClick={() => onClickMenuButton()} to="/loginForm">
+                로그인
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link onClick={onClickLogout} to="/">
+                로그아웃
+              </Link>
+            </li>
+          )}
+
+          <li>
+            <Link onClick={() => onClickMenuButton()} to="/joinForm">
+              회원가입
+            </Link>
+          </li>
+        </ul>
+        <h2>카테고리</h2>
+        <ul className="side-bar-list">
+          {categories &&
+            categories.map((v) => (
+              <li key={v.id}>
+                <Link to="/#">{v.name}</Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="side-bar-curtain" onClick={() => onClickMenuButton()} />
+    </>
+  );
 }
 
-export default SideMenuComponent
+export default SideMenuComponent;
